63ec2bb4dde64582484b4cf4f1c056bb5d9b272f
TheSuMiT <58788722+TheSuMiT0013@users.noreply.github.com>
2019-12-19 00:59:52 +0530

    Update Armstrongno.java

--- Armstrongno.java
+++ Armstrongno.java
@@ -1,10 +1,11 @@
 package javalearning;
+
 import java.util.Scanner;
 
 
 public class Armstrongno {
 
-	public static void main(String[] args)
+	public static void main( String[]  args)
 	{
 		System.out.println("Enter no to check");
 		Scanner obj = new Scanner(System.in);
@@ -15,7 +16,7 @@ public class Armstrongno {
         n=n/10;
 	    int r2 = n%10;
 	     n=n/10;
-	     int r3 = n%10;
+	     int  r3 = n%10;
 	    int ans= (r1*r1*r1)+(r2*r2*r2)+(r3*r3*r3);
 	 
 	    if(ans==n1) 
@@ -43,6 +44,5 @@ public class Armstrongno {
 
 
 
+ 
 
-
-// illahi mera ji aye aye............. :)
