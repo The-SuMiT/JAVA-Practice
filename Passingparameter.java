a1bd1b029bc61175e4c5098276f759e8dfaf2ece
TheSuMiT <58788722+TheSuMiT0013@users.noreply.github.com>
2019-12-19 01:08:39 +0530

    Update Passingparameter.java

--- Passingparameter.java
+++ Passingparameter.java
@@ -1,11 +1,15 @@
 package javalearning;
 /*  Welcome to the Chapter 'Passingparaameter'
- * in the Chapter we are going to learn the passing of parameter
- * thank you.......    :)
- */
+  in the Chapter we are going to learn the passing of parameter to the method
+  thank you.......  
+  
+*/
+
 public class Passingparameter {
 
 	public static void main(String[] args) {
+	
+	system.out.println("This output is because of passing a parameter to the Method  ");
 	int sum	= nav(7,3,3);
 	System.out.println(sum);
 	}
