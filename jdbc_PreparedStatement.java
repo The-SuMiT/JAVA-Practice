fe1c77b605bfecc754f35dd35227f2fab3b7becd
TheSuMiT <58788722+thesumit0069@users.noreply.github.com>
2020-08-17 02:23:13 +0530

    "Calling Mysql connector API for inserting values"
    API used  " mysql connecor/J.jar " 
    PrepearedStatement class is used for executing a SQL statement in jdbc.

--- jdbc_PreparedStatement.java
+++ jdbc_PreparedStatement.java
@@ -18,7 +18,7 @@ public static void main(String[] args)
 	try {
 		Class.forName("com.mysql.jdbc.Driver");
 		 Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/temp","sumit","Ubuntu@007");
-
+ 
 
 		java.sql.PreparedStatement  st = con.prepareStatement(query);
 		// you need to specify that your going to use the java.sql.PreparedStatement class 
