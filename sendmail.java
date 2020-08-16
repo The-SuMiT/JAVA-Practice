32743c2c893f9dd7e3447a57773c8469eb904455
TheSuMiT <58788722+thesumit0069@users.noreply.github.com>
2020-08-17 00:49:35 +0530

    "Sending Gmail from java Program"
    Using Gmail Server  Service we are sending email to particular destination .

---
+++ sendmail.java
@@ -0,0 +1,84 @@
+   import java.util.Properties;    
+   import javax.mail.*;    
+   import javax.mail.internet.*;    
+
+//sending e-mail through Server of Gmail
+
+/*
+
+There are main 3 types of SMTP servers from which you can send mail 
+
+
+1.    Install and use any SMTP server such as Postcast server, Apache James server, cmail server etc.
+2.    Use the SMTP server provided by the host provider e.g. my SMTP server is mail.javatpoint.com
+3.    Use the SMTP Server provided by other companies e.g. gmail etc.
+---------------------------------------------------------------------------------------------------------------
+For sending the email using JavaMail API, you need to load the two jar files:
+1. mail.jar
+2. activation.jar
+
+--------------------------------------------------
+how to compile code in cmd  : 
+
+" javac -cp activation.jar:mail.jar:. sendmail.java "
+//-cp = its shortcut for  setting class path at command line
+//in Linux above code 
+//in win ';' use betwn jar files 7 at end ".;"
+eg.  " javac -cp activation.jar;mail.jar.; sendmail.java "
+-------------------------------------------------------
+how to execute code: 
+" java -cp activation.jar:mail.jar:. sendmail "
+----------------------------------------------------------
+
+*/
+
+class Mailer
+{
+
+
+      public static void send(String from,String password,String to,String sub,String msg){  
+              //Get properties object    
+              Properties props = new Properties();    
+              props.put("mail.smtp.host", "smtp.gmail.com");    
+              props.put("mail.smtp.socketFactory.port", "465");    
+              props.put("mail.smtp.socketFactory.class",    
+                        "javax.net.ssl.SSLSocketFactory");    
+              props.put("mail.smtp.auth", "true");    
+              props.put("mail.smtp.port", "465");    
+
+
+		//get Session   
+              Session session = Session.getDefaultInstance(props,    
+               new javax.mail.Authenticator() {    
+               protected PasswordAuthentication getPasswordAuthentication() {    
+               return new PasswordAuthentication(from,password);  
+               }    
+              });    
+
+
+              //compose message    
+              try {    
+               MimeMessage message = new MimeMessage(session);    
+               message.addRecipient(Message.RecipientType.TO,new InternetAddress(to));
+               message.setSubject(sub);    
+               message.setText(msg);    
+               //send message  
+               Transport.send(message);    
+               System.out.println("message sent successfully");
+              } 
+
+	catch (MessagingException e) {throw new RuntimeException(e);}
+                 
+        }  
+    
+}  
+
+    public class sendmail
+{    
+     public static void main(String[] args)
+ {    
+         //pass parameters below with respective information to there name
+         Mailer.send("(sendermail-id)@gmail.com","(senders_password)","(receivermail-id)@gmail.com"," (Sub of mail) "," (Body of mail) ");  
+
+  }
+}
